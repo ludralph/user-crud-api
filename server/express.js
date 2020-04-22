77d62b95a5500b94ff820118739e7a203ccf83ed
Raphael Etim <raphaelumoh@gmail.com>
2020-04-22 17:14:59 +0100

    add user route middleware

--- server/express.js
+++ server/express.js
@@ -2,21 +2,28 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import compress from 'compression';
 import cors from 'cors';
+import path from 'path';
 import helmet from 'helmet';
+import userRoutes from './routes/user.routes';
 import Template from './../template';
 
 const app = express();
 
+const CURRENT_WORKING_DIR = process.cwd();
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
 app.use(cookieParser());
 app.use(compress());
 app.use(helmet());
 app.use(cors());
 
+app.use('/', userRoutes);
+
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')));
+
 app.get('/', (req,res) => {
     res.status(200).send(Template())
-})
+});
 
 export default app;
