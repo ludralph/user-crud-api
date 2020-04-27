2ecd717e703f3ae33bed9450d54a86a9d62a3ff7
Raphael Etim <raphaelumoh@gmail.com>
2020-04-27 14:14:00 +0100

    add auth middleware and error handler

--- server/express.js
+++ server/express.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 import path from 'path';
 import helmet from 'helmet';
 import userRoutes from './routes/user.routes';
+import authRoutes from './routes/auth.routes';
 import Template from './../template';
 
 const app = express();
@@ -19,11 +20,25 @@ app.use(helmet());
 app.use(cors());
 
 app.use('/', userRoutes);
+app.use('/', authRoutes);
 
 app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')));
 
 app.get('/', (req,res) => {
     res.status(200).send(Template())
 });
+
+app.use((err, req, res, next) => {
+    if(err.name === 'UnauthorizedError'){
+        res.status(401).json({
+            "error": err.name + ": "+ err.message
+        })
+    } else if (err){
+        res.status(400).json({
+            "error": err.name + ": "+ err.message
+        })
+        console.log(err)
+    }
+})
 
 export default app;
