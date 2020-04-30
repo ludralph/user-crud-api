2a01c50b32833f8cb8454114c99cd210aead0789
Raphael Etim <raphaelumoh@gmail.com>
2020-04-30 13:15:54 +0100

    remove console

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -22,7 +22,6 @@ const signin = async (req, res) => {
             }
         })
     } catch (error) {
-        console.log(error)
         return res.status('401').json({error: "Could not sign in"})
     }
 }
